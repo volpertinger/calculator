1d85e2e2181078719977425746c8df8ba223a621
Nikolay <merzlovnik@mail.ru>
2020-05-02 21:00:53 +0300

    done!(+header)

--- main.cpp
+++ main.cpp
@@ -2,9 +2,4 @@
 #include <iostream>
 #include <string>
 
-int main() {
-  std::string str = "(1+4-10)*cos(0)";
-  solve(str);
-  std::cout << str << std::endl;
-  return 0;
-}
+int main() { return 0; }
